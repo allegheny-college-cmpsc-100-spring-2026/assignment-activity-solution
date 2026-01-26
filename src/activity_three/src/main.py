44ceeba9e03dc31b12658fa597d3ddad7b641d0d
dluman <dluman@douglasjluman.com>
2026-01-26 10:42:42 -0500

    Ok...making a it a loop

--- src/activity_three/src/main.py
+++ src/activity_three/src/main.py
@@ -16,28 +16,28 @@ def make_progressive(guess: int = 1, answer: int = 1) -> int:
 def main(answer: int = 1, low: int = 1, high: int = 100, count: int = 1):
     again = True
     right = False
-    
-    guess = int(input(f"Guess a number between {low}-{high}: "))
-    # Evaluate answer and guess
-    if guess < answer:
-        print(message(text = "too low"))
-    elif guess > answer:
-        print(message(text = "too high"))
-    else:
-        print(message())
-        right = True
 
-    # Evaluate right/wrong, and playing again
-    if right:
-        print(f"It took you {count} guess(es)")
-        count = 0
-        again = input("Guess again (y/n)? ").lower() == "y"
-        answer = generate(low = low, high = high)
-    else:
-        count += 1
-        answer = make_progressive(guess = guess, answer = answer)
-    if again:
-        main(answer = answer, count = count)
+    while again:
+        guess = int(input(f"Guess a number between {low}-{high}: "))
+        # Evaluate answer and guess
+        if guess < answer:
+            print(message(text = "too low"))
+        elif guess > answer:
+            print(message(text = "too high"))
+        else:
+            print(message())
+            right = True
+
+        # Evaluate right/wrong, and playing again
+        if right:
+            print(f"It took you {count} guess(es)")
+            count = 0
+            right = False
+            again = input("Guess again (y/n)? ").lower() == "y"
+            answer = generate(low = low, high = high)
+        else:
+            count += 1
+            answer = make_progressive(guess = guess, answer = answer)
     
 
 if __name__ == "__main__":
